7f97dfb352214d202e6728ee2002fabaf268f754
hjk33433 <1774958630@qq.com>
2022-03-25 19:37:07 +0800

    Initial commit

---
+++ src/main/java/com/example/slowvideoweb/SlowVideoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.slowvideoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowVideoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlowVideoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slowvideoweb/SlowVideoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slowvideoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowVideoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
